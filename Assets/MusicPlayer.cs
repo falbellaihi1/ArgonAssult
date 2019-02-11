64eb918c50e572c5d64bcf1843362e986d13d35e
falbellaihi <falbellaihi@hotmail.com>
2019-02-11 21:05:11 +0300

    Music

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+    }
+    void Start()
+    {
+        Invoke("LoadNextScene", 2f); ;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+
+    private void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+       
+        SceneManager.LoadScene(nextSceneIndex);
+
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+      
+        }
+      
+    }
+}
