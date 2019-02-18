072abd76a6a2e4ee88de38eafdcb0411d74144df
falbellaihi <falbellaihi@hotmail.com>
2019-02-18 07:46:20 +0300

    Added enemies, fixed controls

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,8 +5,15 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
-    [Tooltip("In ms^-1")][SerializeField] float TurnSpeed = 4f;
-    [Tooltip("In m")] [SerializeField] float TurnRange = 5f;
+    [Tooltip("In ms^-1")][SerializeField] float TurnSpeed = 20f;
+    [Tooltip("In m")] [SerializeField] float TurnRange = 4f;
+    [SerializeField] float positionPitchFactor = -5f;
+    [SerializeField] float controlPitchFactor = -20f;
+    [SerializeField]float positionYawFactor = 25f;
+    [SerializeField] float positionRollFactor = -20f;
+    float xThrow, yThrow;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,25 +23,52 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ProcessTranslation();
+        RotateShip();
 
-        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
-        float yLocalPos = transform.localPosition.y;
-        float xLocalPos = transform.localPosition.x;
-       
 
-        transform.localPosition = new Vector3(CalculateTurn(xThrow,xLocalPos), CalculateTurn(yThrow, yLocalPos), transform.localPosition.z);
-
-        
     }
 
-    float CalculateTurn(float Throw, float localPos)
+    private void ProcessTranslation()
     {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yLocalPos = transform.localPosition.y;
+        float xLocalPos = transform.localPosition.x;
+        transform.localPosition = new Vector3(CalculateTurn(xThrow, xLocalPos, false), CalculateTurn(yThrow, yLocalPos, true), transform.localPosition.z);
+
+    }
+
+    float CalculateTurn(float Throw, float localPos, bool isY)
+    {
+        if (isY)
+        {
+            TurnRange = 3.5f;
+        }
+        else
+        {
+            TurnRange = 4f;
+        }
         float ClampedPos;
         float Offset = Throw * TurnSpeed * Time.deltaTime;
         float rowNewPos = localPos + Offset;
         ClampedPos = Mathf.Clamp(rowNewPos, -TurnRange, TurnRange);
+        
+        
         return ClampedPos;
+    }
+   
+
+    void RotateShip()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+
+        float yaw = transform.localPosition.x * positionYawFactor;
+
+        float roll = xThrow * positionRollFactor;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 
 }
